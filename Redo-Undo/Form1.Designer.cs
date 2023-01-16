b1afcae45e9e7b51425741be6fab317fb84c125c
Yusuf Fakhreddin <48655310+Yusuf-Fakhreddin@users.noreply.github.com>
2023-01-16 17:31:58 +0200

    added GUI elements and functionality

--- Redo-Undo/Form1.Designer.cs
+++ Redo-Undo/Form1.Designer.cs
@@ -28,13 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.btn_undo = new System.Windows.Forms.Button();
+            this.btn_redo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textOutput
+            // 
+            this.textOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textOutput.Location = new System.Drawing.Point(28, 29);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.Size = new System.Drawing.Size(584, 60);
+            this.textOutput.TabIndex = 0;
+            this.textOutput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btn_undo
+            // 
+            this.btn_undo.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.btn_undo.Location = new System.Drawing.Point(76, 113);
+            this.btn_undo.Name = "btn_undo";
+            this.btn_undo.Size = new System.Drawing.Size(176, 66);
+            this.btn_undo.TabIndex = 1;
+            this.btn_undo.Text = "Undo";
+            this.btn_undo.UseVisualStyleBackColor = true;
+            this.btn_undo.Click += new System.EventHandler(this.btn_undo_Click);
+            // 
+            // btn_redo
+            // 
+            this.btn_redo.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.btn_redo.Location = new System.Drawing.Point(381, 113);
+            this.btn_redo.Name = "btn_redo";
+            this.btn_redo.Size = new System.Drawing.Size(183, 66);
+            this.btn_redo.TabIndex = 2;
+            this.btn_redo.Text = "Redo";
+            this.btn_redo.UseVisualStyleBackColor = true;
+            this.btn_redo.Click += new System.EventHandler(this.btn_redo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(654, 349);
+            this.Controls.Add(this.btn_redo);
+            this.Controls.Add(this.btn_undo);
+            this.Controls.Add(this.textOutput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textOutput;
+        private System.Windows.Forms.Button btn_undo;
+        private System.Windows.Forms.Button btn_redo;
     }
 }
 
--- Redo-Undo/Form1.cs
+++ Redo-Undo/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Collections;
 namespace Redo_Undo
 {
     public partial class Form1 : Form
@@ -15,6 +15,34 @@ namespace Redo_Undo
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //LIFO (Stack)
+        Stack st = new Stack();
+        private void btn_undo_Click(object sender, EventArgs e)
+        {
+            // push last char to stack 
+            // remove last char from text output
+            if (textOutput.Text.Length > 0)
+            {
+                int LastIndex = textOutput.Text.Length - 1;
+                string LastChar = textOutput.Text.Substring(LastIndex, 1);
+                textOutput.Text = textOutput.Text.Substring(0, LastIndex);
+                st.Push(LastChar);
+            }
+        }
+
+        private void btn_redo_Click(object sender, EventArgs e)
+        {
+            if (st.Count > 0)
+            {
+                textOutput.Text += st.Pop();
+            }
         }
     }
 }
